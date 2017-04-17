7d5bedf8f9ded7f3dd8ed4c9b946e425047a07b0
Neota Moe <neota.moe@gmail.com>
2017-04-16 20:15:57 -0500

    countdown animate complete

--- pro_practice/script.js
+++ pro_practice/script.js
@@ -8,3 +8,30 @@
   BONUS 1: Add a button for the user to reset the timer.
   BONUS 2: Make something cool happen using a jQuery animation when the timer gets to zero.
 */
+
+$(document).ready(timer);
+
+
+function timer (){
+  var counter = $('.count').text();
+  setInterval(function(){
+    counter--;
+    if (counter<1){
+      $('.count').text(0);
+      $('div').animate({
+        fontSize: '200px',
+      }, "slow");
+      clearInterval(timer);
+    } else {
+      $('.count').text(counter);
+    }
+  }, 1000);
+}
+
+// function resetCounter(){
+//   var counterResetValue=$('.count').text(10);
+//   counter=counterResetValue;
+// }
+
+
+$('#button').on('click', clearInterval(timer));
